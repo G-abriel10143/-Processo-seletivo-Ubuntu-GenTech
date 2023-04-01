ebc9d487cec6e06a361d834a2fbaec33a725411c
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2023-03-31 22:32:40 -0300

    terminado

--- Gabriel/js/script.js
+++ Gabriel/js/script.js
@@ -1,22 +1,37 @@
-const carousel = document.querySelector('.carousel-items');
-const prevButton = document.querySelector('.carousel-prev');
-const nextButton = document.querySelector('.carousel-next');
+const carouselItems = document.querySelector('.carousel-items');
+const carouselItem = document.querySelectorAll('.carousel-item');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
 
-let position = 0;
-const slideWidth = carousel.clientWidth / 3;
-const slidesToScroll = 1;
+let index = 0;
+const totalItems = carouselItem.length;
 
-prevButton.addEventListener('click', () => {
-  const slidesToShow = Math.min(carousel.childElementCount, 3);
-  position += slidesToScroll * slideWidth;
-  position = Math.min(position, 0);
-  carousel.style.transform = `translateX(${position}px)`;
+carouselNext.addEventListener('click', () => {
+  index++;
+  if (index > totalItems - 1) {
+    index = 0;
+  }
+  carouselItems.style.transform = `translateX(-${index * 20}%)`;
 });
 
-nextButton.addEventListener('click', () => {
-  const slidesToShow = Math.min(carousel.childElementCount, 3);
-  position -= slidesToScroll * slideWidth;
-  const lastPosition = -(carousel.childElementCount - slidesToShow) * slideWidth;
-  position = Math.max(position, lastPosition);
-  carousel.style.transform = `translateX(${position}px)`;
+carouselPrev.addEventListener('click', () => {
+  index--;
+  if (index < 0) {
+    index = totalItems - 1;
+  }
+  carouselItems.style.transform = `translateX(-${index * 20}%)`;
 });
+function showItem(index) {
+  const items = document.querySelectorAll('.carousel-item');
+  const dots = document.querySelectorAll('.carousel-dots span');
+
+  items.forEach(item => {
+    item.classList.remove('active');
+  });
+  dots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+
+  items[index].classList.add('active');
+  dots[index].classList.add('active');
+}
